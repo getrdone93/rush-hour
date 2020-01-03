31fb87d4a22478d1d8efc9f801bce4cdea65a0df
Trever Anderson <treveranderson01@gmail.com>
2020-01-02 20:24:44 -0600

    init commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to rush-hour
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/rush_hour/core_test.clj
@@ -0,0 +1,7 @@
+(ns rush-hour.core-test
+  (:require [clojure.test :refer :all]
+            [rush-hour.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
