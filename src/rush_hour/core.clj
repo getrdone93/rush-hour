a2bfa1950a1ec7e4df9b4a1fbcb3655648b6a2e4
Trever Anderson <treveranderson01@gmail.com>
2020-01-07 19:47:08 -0600

    refactor

--- src/rush_hour/core.clj
+++ src/rush_hour/core.clj
@@ -7,6 +7,26 @@
  '(javax.swing JPanel JFrame))
 
 (def grid-dims {:dim 800 :num-sqs 6 :sq-dim 100})
+(def board-meta-data {:vehicle {:x {:color :ff0000 :type :car :location [[] []]}
+                                :a {:color :60d700 :type :car :location [[] []]}
+                                :b {:color :ff9e13 :type :car :location [[] []]}
+                                :c {:color :05e2f6 :type :car :location [[] []]}
+                                :d {:color :ff75da :type :car :location [[] []]}
+                                :e {:color :4640bf :type :car :location [[] []]}
+                                :f {:color :3b7b39 :type :car :location [[] []]}
+                                :g {:color :8a8989 :type :car :location [[] []]}
+                                :h {:color :c18862 :type :car :location [[] []]}
+                                :i {:color :fff837 :type :car :location [[] []]}
+                                :j {:color :532d00 :type :car :location [[] []]}
+                                :k {:color :5a9f17 :type :car :location [[] []]}
+                                :o {:color :c1bc32 :type :truck :location [[] []]}
+                                :p {:color :b71bff :type :truck :location [[] []]}
+                                :q {:color :256dff :type :truck :location [[] []]}
+                                :r {:color :0eae92 :type :truck :location [[] []]}}})
+
+(defn gen-board [d]
+  (mapv #(vec %)
+        (partition d (take (* d d) (repeat {:vehicle :open})))))
 
 (defn draw-grid [im-graph bd draw-x draw-y border-thick sq-dim]
   (if (< draw-x bd)
@@ -32,27 +52,6 @@
              (doto (proxy [JPanel] []
                         (paint [g] (color-frame g grid-dims)))
                  (.setPreferredSize (new Dimension dim dim)))))
-
-(def board-meta-data {:vehicle {:x {:color :ff0000 :type :car :location [[] []]}
-                                :a {:color :60d700 :type :car :location [[] []]}
-                                :b {:color :ff9e13 :type :car :location [[] []]}
-                                :c {:color :05e2f6 :type :car :location [[] []]}
-                                :d {:color :ff75da :type :car :location [[] []]}
-                                :e {:color :4640bf :type :car :location [[] []]}
-                                :f {:color :3b7b39 :type :car :location [[] []]}
-                                :g {:color :8a8989 :type :car :location [[] []]}
-                                :h {:color :c18862 :type :car :location [[] []]}
-                                :i {:color :fff837 :type :car :location [[] []]}
-                                :j {:color :532d00 :type :car :location [[] []]}
-                                :k {:color :5a9f17 :type :car :location [[] []]}
-                                :o {:color :c1bc32 :type :truck :location [[] []]}
-                                :p {:color :b71bff :type :truck :location [[] []]}
-                                :q {:color :256dff :type :truck :location [[] []]}
-                                :r {:color :0eae92 :type :truck :location [[] []]}}})
-
-(defn gen-board [d]
-  (mapv #(vec %)
-        (partition d (take (* d d) (repeat {:vehicle :open})))))
 
 (defn frame [] (doto
                  (new JFrame)
