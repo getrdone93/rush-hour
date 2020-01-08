7a694493ab06f2fb5ad543333a1c167e2ec1a481
Trever Anderson <treveranderson01@gmail.com>
2020-01-07 19:46:10 -0600

    put in cars and initial board structure

--- src/rush_hour/core.clj
+++ src/rush_hour/core.clj
@@ -6,7 +6,7 @@
  '(java.awt.image BufferedImage)
  '(javax.swing JPanel JFrame))
 
-(def board-stats {:dim 800 :num-sqs 6 :sq-dim 100})
+(def grid-dims {:dim 800 :num-sqs 6 :sq-dim 100})
 
 (defn draw-grid [im-graph bd draw-x draw-y border-thick sq-dim]
   (if (< draw-x bd)
@@ -28,10 +28,31 @@
        (. g (drawImage img 0 0 nil))
        (. im-graph (dispose))))
 
-(def panel (let [dim (board-stats :dim)]
+(def panel (let [dim (grid-dims :dim)]
              (doto (proxy [JPanel] []
-                        (paint [g] (color-frame g board-stats)))
+                        (paint [g] (color-frame g grid-dims)))
                  (.setPreferredSize (new Dimension dim dim)))))
+
+(def board-meta-data {:vehicle {:x {:color :ff0000 :type :car :location [[] []]}
+                                :a {:color :60d700 :type :car :location [[] []]}
+                                :b {:color :ff9e13 :type :car :location [[] []]}
+                                :c {:color :05e2f6 :type :car :location [[] []]}
+                                :d {:color :ff75da :type :car :location [[] []]}
+                                :e {:color :4640bf :type :car :location [[] []]}
+                                :f {:color :3b7b39 :type :car :location [[] []]}
+                                :g {:color :8a8989 :type :car :location [[] []]}
+                                :h {:color :c18862 :type :car :location [[] []]}
+                                :i {:color :fff837 :type :car :location [[] []]}
+                                :j {:color :532d00 :type :car :location [[] []]}
+                                :k {:color :5a9f17 :type :car :location [[] []]}
+                                :o {:color :c1bc32 :type :truck :location [[] []]}
+                                :p {:color :b71bff :type :truck :location [[] []]}
+                                :q {:color :256dff :type :truck :location [[] []]}
+                                :r {:color :0eae92 :type :truck :location [[] []]}}})
+
+(defn gen-board [d]
+  (mapv #(vec %)
+        (partition d (take (* d d) (repeat {:vehicle :open})))))
 
 (defn frame [] (doto
                  (new JFrame)
